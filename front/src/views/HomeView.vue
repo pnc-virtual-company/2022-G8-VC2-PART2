1e0cf458f84f1a8f4a6e15a04ef61f367f6c5be7
radytrainer <rady.y@passerellesnumeriques.org>
2022-08-30 10:00:06 +0700

    #0 Project Initialize

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" width="100">
+    <HelloWorld msg="WELCOME TO VIRTUAL COMPANY II 2022"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
