4bc21d00adf852c3fc3fd4e7af18cbe07e5fa96f
SokunbophaSovann2003 <sokunbophasovann@gmail.com>
2022-09-17 20:34:49 +0700

    UPDATE CREATE STUDENT #4 CREATE STUDENT ACCOUNT

--- front/src/components/Cordinator/StudentList/CreateStudent.vue
+++ front/src/components/Cordinator/StudentList/CreateStudent.vue
@@ -2,10 +2,30 @@
     <div  v-if="storeData.isTrue" class="z-10">  
       <div class="mt-[-12] fixed w-full h-full inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50">
         <form @submit.prevent="storeData.createStudent"  class="bg-white text-center p-5 w-2/5 m-auto rounded">
-           <div  class="w-1/4  rounded-full m-auto">
-            <label for="image">
-              <img v-if="storeData.previewImage != null" :src="storeData.previewImage"  class="w-6/6 h-28 rounded-full m-auto object" alt="">
-              <img v-if="storeData.previewImage == null" src="../../../assets/male_logo.jpg" class="w-6/6 rounded-full m-auto" alt="" />
+          <!-- ==================== User on action cancel ==================== -->
+          <div class="flex justify-end">
+            <span @click="storeData.onCancel" class="text-red cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </span>
+          </div>
+
+          <div  class="w-28 h-28 rounded-full m-auto object cursor-pointer">
+            <label for="image" class="w-28 h-28 rounded-full m-auto object cursor-pointer">
+              <img v-if="storeData.previewImage != null" :src="storeData.previewImage"  class="w-28 h-28 rounded-full m-auto object" alt="">
+              <img v-if="storeData.previewImage == null" src="../../../assets/male_logo.jpg" class="w-28 h-28 rounded-full m-auto" alt="" />
             </label>
             <input type="file" @change="storeData.uploadImage" hidden id="image">
            </div>
@@ -35,7 +55,6 @@
             <input v-model="storeData.class"  type="text" class="m-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="class">
             <input v-model="storeData.studentNumber"  type="text" class="m-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="student ID">
           </div>
-          <button @click="storeData.onCancel" class="bg-red-500 p-1.5 text-white mr-1 rounded w-20 mt-10" >Cancel</button>
           <button  class=" p-1.5 text-white bg-sky-500 mr-1 rounded w-20 mt-10">Create</button>
         </form>
       </div>
